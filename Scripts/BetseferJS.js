305fb760422720c1a26364f4ef2110be86a543d6
Ellis Barovsky <33927309+elisBarovsky@users.noreply.github.com>
2018-03-15 11:29:34 +0200

    decoration nav bar

--- Scripts/BetseferJS.js
+++ Scripts/BetseferJS.js
@@ -1,5 +1,63 @@
-﻿$(document).ready(function () {
+﻿$('#LoginPage').ready(function () {
    // alert(localStorage.getItem("UserID")); // how to get the local storage 
+    $(".navbar-header").hide(); 
+
+
+//$('#slide-nav.navbar-inverse').after($('<div class="inverse" id="navbar-height-col"></div>'));
+//$('#slide-nav.navbar-default').after($('<div id="navbar-height-col"></div>'));
+
+//var slidewidth = '20%';
+//var navbarneg = '-' + slidewidth;
+
+//if ($(window).width() < 767) {
+//    $('#navbar-height-col').css("width", slidewidth);
+//    $('#navbar-height-col').css("right", navbarneg);
+//    $('#slide-nav #slidemenu').css("width", slidewidth);
+//    $('#slide-nav #slidemenu').css("right", navbarneg);
+//}
+
+//$("#slide-nav").on("click", '.navbar-toggle', function (e) {
+
+//    // slider is active
+//    var selected = $(this).hasClass('slide-active');
+
+//    // set slidemenu width
+//    $('#slidemenu').stop().animate({
+//        right: selected ? navbarneg : '0px'
+//    });
+
+//    // set navbar width
+//    $('#navbar-height-col').stop().animate({
+//        right: selected ? navbarneg : '0px'
+//    });
+
+//    // set content let
+//    $('#page-content').stop().animate({
+//        right: selected ? '0px' : slidewidth
+//    });
+
+//    // set navbar left
+//    $('.navbar-header').stop().animate({
+//        right: selected ? '0px' : slidewidth
+//    });
+
+//    $(this).toggleClass('slide-active', !selected);
+//    $('#slidemenu').toggleClass('slide-active');
+
+//    $('#page-content, .navbar, body, .navbar-header').toggleClass('slide-active');
+//});
+
+//var selected = '#slidemenu, #page-content, body, .navbar, .navbar-header';
+
+//$(window).on("resize", function () {
+//    if ($(window).width() > 767 && $('.navbar-toggle').is(':hidden')) {
+//        $(selected).removeClass('slide-active');
+//    }
+//});
+});
+
+$(document).on("pageinit", "#DashBordPage", function (event) {
+    $(".navbar-header").show();
 
 $('#slide-nav.navbar-inverse').after($('<div class="inverse" id="navbar-height-col"></div>'));
 $('#slide-nav.navbar-default').after($('<div id="navbar-height-col"></div>'));
@@ -11,7 +69,8 @@ if ($(window).width() < 767) {
     $('#navbar-height-col').css("width", slidewidth);
     $('#navbar-height-col').css("right", navbarneg);
     $('#slide-nav #slidemenu').css("width", slidewidth);
-    $('#slide-nav #slidemenu').css("right", navbarneg);
+    $('#slide-nav #slidemenu').css("z-index", 5000);
+    $('#slide-nav #slidemenu').css("z-index", 5000);
 }
 
 $("#slide-nav").on("click", '.navbar-toggle', function (e) {
