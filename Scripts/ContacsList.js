66366cde40c14a5100a8da2b1b529a02c012ff76
Ellis Barovsky <33927309+elisBarovsky@users.noreply.github.com>
2018-06-14 15:30:26 +0300

    חיוג עובד

--- Scripts/ContacsList.js
+++ Scripts/ContacsList.js
@@ -17,41 +17,27 @@ function onDeviceReady() {
         User.type = 3;
         FillCelphoneByTypeAndPupilId(User, FillListViewCellPhone);
     });
-
-    //$('.tele').click(function () {
-    //    alert(123);
-    //    debugger;
-    //    var tele = $(this).attr('tele');
-    //    //var tele = '0525382634';
-    //    window.open('tel:' + tele, '_system', 'location=yes');
-    //});
 }
 
-function func1(tele) {
-    debugger;
-   
-    alert(tele);
+function funcToCall(tele) {
+    tele = tele.toString().substring(2);
     
-    window.open('tel:' + tele, '_system', 'location=yes');
+   window.open('tel:' + tele, '_system', 'location=yes');
 }
 
 function FillListViewCellPhone(results) {  //contactList
     res = $.parseJSON(results.d);
-    var counter = 0;
+    counter = 0;
     var phoneIcon = "Images/PhoneIcon.png";
 
     $('#contactList').empty();
-//    dynamicLy = "<ul style='list-style-type:none'>";
+
     for (var i = 0; i < res.length; i++) { //ממלא את הרשימה בילדים של ההורה
 
-        //<input id="2" src="Images/PhoneIcon.png" onclick="window.open('tel:0525382634', '_system', 'location=yes')" type='image' height='25' style='float: right' />
-
-        dynamicLy = "<li  class='tele' onclick='func1(" + String('0-1234567') + ")' ><p style='margin-left:100px'><center><input id='" + i +
-            "' src='" + phoneIcon + "' tele=" + res[counter].PhoneNumber  + " type='image'  height='25' style='float: right' /> &nbsp;" +
+        dynamicLy = "<li  class='tele' onclick='funcToCall(." + res[counter].PhoneNumber + ")' ><p style='margin-left:100px'><center><input id='" + i +
+            "' src='" + phoneIcon + "' type='image'  height='25' style='float: right' /> &nbsp;" +
             res[counter].FullName + " &nbsp;&nbsp;" + res[counter].PhoneNumber + "</center> </p> </li>";
         counter++;
         $('#contactList').append(dynamicLy);
     }
-    //dynamicLy += "</ul>";
-  //  $('#contactList').selectmenu('refresh');
 }
