0764f0f2ebff6fba31c3597c60dceceb641e4653
Ellis Barovsky <33927309+elisBarovsky@users.noreply.github.com>
2018-06-23 09:30:13 +0300

    .

--- Scripts/Parent_Dashboard.js
+++ Scripts/Parent_Dashboard.js
@@ -5,26 +5,28 @@ function onDeviceReady() {
 
     $('body').fadeIn(500, function () {
 
- $('#Logout').click(function () {
-     swal({
-         title: "כבר עוזבים? :(",
-         icon: "warning",
-         buttons: ["לא", "כן"],
-         dangerMode: true,
-     })
-         .then((willDelete) => {
-             if (willDelete) {
-                 swal("תחזור בקרוב!", {
-                     icon: "success",
-                 });
-                 window.location.href = "index.html"
+        $('#Logout').click(function () {
 
-             } else {
-                 swal("יש נשארת!");
-             }
-         });
-    });
+            swal({
+                title: "כבר עוזבים? :(",
+                icon: "warning",
+                buttons: ["לא", "כן"],
+                dangerMode: true,
+            })
+                .then((willDelete) => {
+                    if (willDelete) {
+                        swal("תחזור בקרוב!", {
+                            icon: "success",
+                        });
+                        setTimeout(function () {
+                            window.location.href = "index.html"
 
+                        }, 1000);
+
+                    } else {
+                        swal("יש נשארת!");
+                    }
+                });
     });
    
 }
