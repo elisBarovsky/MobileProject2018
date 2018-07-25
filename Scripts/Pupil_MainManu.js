ddf65c788572969f9817dbddbe26451a25cb1c5c
Dandik100 <33927352+Dandik100@users.noreply.github.com>
2018-07-25 20:22:18 +0300

    gjgh

--- Scripts/Pupil_MainManu.js
+++ Scripts/Pupil_MainManu.js
@@ -59,15 +59,15 @@ function ClassAvgGrades(results) {
 
     var PupilIFullName = localStorage.getItem("UserFullName");
     if (place == 1) {
-        document.getElementById("UserNameLBL").innerHTML = " שלום " + PupilIFullName + " <img src='Images/gold.png'  height='70' style='float:left' />";
+        document.getElementById("UserNameLBL").innerHTML = PupilIFullName + " <img src='Images/gold.png'  height='70' style='float:left' />";
 
     }
     else if (place == 2) {
-        document.getElementById("UserNameLBL").innerHTML = " שלום " + PupilIFullName + " <img src='Images/Silver.png'  height='70' style='float:left' />";
+        document.getElementById("UserNameLBL").innerHTML = PupilIFullName + " <img src='Images/Silver.png'  height='70' style='float:left' />";
 
     }
     else if (place == 3) {
-        document.getElementById("UserNameLBL").innerHTML = " שלום " + PupilIFullName + " <img src='Images/bronze.png' height='70' style='float:left' />";
+        document.getElementById("UserNameLBL").innerHTML = PupilIFullName + " <img src='Images/bronze.png' height='70' style='float:left' />";
 
     }
     
